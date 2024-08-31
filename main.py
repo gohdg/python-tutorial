3f9942e5cb7b6826579798375892be893ffa22be
gohdg <gohdg@naver.com>
2024-08-31 13:29:39 +0900

    if statement

---
+++ main.py
@@ -0,0 +1,29 @@
+# if = Do some code only IF some condtion is True
+#      Else do something else
+
+# age = int(input("Enter your age: "))
+
+# if age >= 100:
+#     print("You are too old to sign up")
+# elif age >= 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't been born yet!")
+# else:
+#     print("You must be 18+ to sign up")
+
+
+name = input("Enter your name: ")
+
+if name != "":
+    print(f"Hello {name}")
+else:
+    print("You did not type in your name!")
+
+
+# for_sale = False
+
+# if for_sale:
+#     print("This item is for sale")
+# else:
+#     print("This item is NOT for sale")
