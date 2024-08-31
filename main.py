c2073167a09cb99e5cfef17ea1482eddb561b17a
gohdg <gohdg@naver.com>
2024-08-31 19:35:02 +0900

    이자계산기

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-# while loop: 조건이 true일 동안 코드 실행
+# Python compound interest calculator
 
-# name = input("Enter your name: ")
+# 이자계산
 
-# while name == "":
-#     print("You did not enter your name")
-#     name = input("Enter your name: ")
+principle = 0
+rate = 0
+time = 0
 
-# print(f"Hello {name}")
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle < 0:
+        print("Principle can't be less than zero")
+    else:
+        # while loop 탈출
+        break
 
+while True:
+    rate = float(input("Enter the interest rate: "))
+    if rate < 0:
+        print("Rate can't be less than zero")
+    else:
+        # while loop 탈출
+        break
 
-# age = int(input("Enter your age: "))
+while True:
+    time = float(input("Enter the time in years: "))
+    if time < 0:
+        print("Time can't be less than zero")
+    else:
+        # while loop 탈출
+        break
 
-# while age < 0:
-#     print("Age can't be negative")
-#     age = int(input("Enter your age: "))
-
-# print(f"You are {age} years old")
-
-
-# food = input("Enter a food you like ( q to quit):")
-
-# # while not food == "q":
-# while food != "q":
-#     print(f"You like {food}")
-#     food = input("Enter another food you like ( q to quit):")
-
-# print("bye")
-
-num = int(input("Enter a # between 1 - 10: "))
-
-while num < 1 or num > 10:
-    print(f"{num} is not valid")
-    num = int(input("Enter a # between 1 - 10: "))
-
-print(f"Your number is {num}")
+total = principle * pow((1+rate/100), time)
+print(f"Balance after {time} years: ${total:,.2f}")
