e01c71bb79c2f98f28ac4066ae68865f8858b84d
gohdg <gohdg@naver.com>
2024-08-31 19:55:34 +0900

    for loop

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-# Python compound interest calculator
+# for loops
+# 형식: for x in range(1,11):
+# 유용함수 range(start, end(exclusive),[step])
+# continue - skip, break - loop 탈출
 
-# 이자계산
 
-principle = 0
-rate = 0
-time = 0
+# 1,2,3,4,5,6,~10
+# for x in range(1, 11):
+#     print(x)
 
-while True:
-    principle = float(input("Enter the principle amount: "))
-    if principle < 0:
-        print("Principle can't be less than zero")
+# 10,9,8,7,.. 1
+# for x in reversed(range(1, 11)):
+#     print(x)
+
+# print("HAPPY NEW YEAR!")
+
+# 1,3,5,... 9
+# for x in range(1, 11, 2):
+#     print(x)
+
+# List
+# items = [1, 2, 33, 56, 78]
+# for item in items:
+#     print(item)
+
+# String
+# card_number = "1234-5678-9099-3456"
+# for x in card_number:
+#     print(x)
+
+# Skip - continue
+# for x in range(1, 21):
+#     if x == 13:
+#         continue # 13일때 14로 바로 넘어간다
+#     else:
+#         print(x)
+
+# Exit - break
+for x in range(1, 21):
+    if x == 13:
+        break  # 13일때 탈출
     else:
-        # while loop 탈출
-        break
-
-while True:
-    rate = float(input("Enter the interest rate: "))
-    if rate < 0:
-        print("Rate can't be less than zero")
-    else:
-        # while loop 탈출
-        break
-
-while True:
-    time = float(input("Enter the time in years: "))
-    if time < 0:
-        print("Time can't be less than zero")
-    else:
-        # while loop 탈출
-        break
-
-total = principle * pow((1+rate/100), time)
-print(f"Balance after {time} years: ${total:,.2f}")
+        print(x)
