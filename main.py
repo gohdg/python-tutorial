18344e99288bb8c2442f94032b412ab60445e9c9
gohdg <gohdg@naver.com>
2024-09-04 15:30:34 +0900

    polymorphism

--- main.py
+++ main.py
@@ -1,68 +1,55 @@
-# super() = Function used in a child class to call methods from a parent class (super class).
-#           Allows you to extend the functionality of the inherited methods
+# Polymorphism = Greek word that means to "have many forms or faces"
+#                Poly = Many
+#                Morphe = Form
 
-from turtle import circle, width
+#               TWO WAYS TO ACHIEVE POLYMORPHISM
+#               1. Inheritance = An object could be treated of the same type as a parent class
+#               2. "Duck typing" = Object must have necessary attributes/methods
+
+# 각각의 클래스가 동일 이름의 메서드를 가지고 있을때 각 클래스를 List화 해서 for loop로 호출하는 것
+
+from abc import ABC, abstractmethod
 
 
-class Shape:
-    def __init__(self, color, is_filled) -> None:
-        self.color = color
-        self.is_filled = is_filled
+class Shape(ABC):  # ABC를 상속 받으면 추상 클래스가 된다.
 
-    def describe(self):
-        print(f"It is {self.color} and {
-              'filled' if self.is_filled else 'not filled'}")
+    @abstractmethod  # abstratmethod decorator 를 선언 sub class는 반드시 구현해야 한다
+    def area(self):
+        pass
 
 
 class Circle(Shape):
-    def __init__(self, color, is_filled, radius) -> None:
-        super().__init__(color, is_filled)
+    def __init__(self, radius) -> None:
         self.radius = radius
-    # override
 
-    def describe(self):
-        super().describe()  # extend functionality
-        print(f"It is a circle with an area of {
-              3.14 * self.radius * self.radius}")
+    def area(self):
+        return 3.14 * self.radius ** 2
 
 
 class Square(Shape):
-    def __init__(self, color, is_filled, width) -> None:
-        super().__init__(color, is_filled)
-        # self.color = color
-        # self.filled =filled
-        self.width = width
+    def __init__(self, side) -> None:
+        self.side = side
 
-    def describe(self):
-        super().describe()  # extend functionality
-        print(f"It is a square with an area of {
-              self.width * self.width}")
+    def area(self):
+        return self.side ** 2
 
 
 class Triangle(Shape):
-    def __init__(self, color, is_filled, width, height) -> None:
-        super().__init__(color, is_filled)
-        # self.color = color
-        # self.filled =filled
-        self.width = width
+    def __init__(self, base, height) -> None:
+        self.base = base
         self.height = height
 
-    def describe(self):
-        super().describe()  # extend functionality
-        print(f"It is a triangle with an area of {
-              self.width * self.height / 2}")
+    def area(self):
+        return self.base * self.height * 0.5
 
 
-circle = Circle("red", True, 5)
-print(circle.color)
+class Pizza(Circle):  # Circle 을 상속하면 area 메소드를 사용할 수 있게 되어 아래 list에 담아 사용할 수 있어 Circle를 상속 받음
+    def __init__(self, topping, radius) -> None:
+        super().__init__(radius)
+        self.topping = topping
 
-square = Square(color="blue", is_filled=False, width=6)
-print(square.color)
 
-triangle = Triangle(color="yellow", is_filled=True, width=7, height=8)
-print(triangle.width)
-print(triangle.height)
+shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
 
-circle.describe()
-square.describe()
-triangle.describe()
+for shape in shapes:
+    print(f"{shape.area()}cm²")
