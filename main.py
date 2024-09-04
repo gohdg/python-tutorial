a5eac78180390d4a0d2a93f956c25fa8aed2565c
gohdg <gohdg@naver.com>
2024-09-04 10:57:22 +0900

    Inheritance

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
-# class variables = Shared among all instance of a class
-#                   Defined outside the constructor
-#                   Allow you to share data among all objects created from that class
+# Inheritance = Allows a class to inherit attributes and methods from another class
+#               Helps with code reusability and extensibility
+#               class Child(Parent)
 
-class Student:
-    class_year = 2024
-    num_students = 0
+class Animal:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.is_alive = True
 
-    def __init__(self, name, age) -> None:
-        self.name = name  # instance variable
-        self.age = age  # instance variable
-        Student.num_students += 1
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
 
 
-student1 = Student("Spongebob", 30)
-student2 = Student("Patrick", 35)
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
 
-# class 변수 접근은 intance에서도 접근 되고, 클래스명으로도 접근된다.
-# class 변수 접근법: instance.클래스 변수
-print(student1.name)
-print(student1.age)
-print(student1.class_year)  # 2024, 공유되고 있다
 
-print(student2.name)
-print(student2.age)
-print(student2.class_year)  # 2024, 공유되고 있다
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
 
-# class 변수 접근법: 클래스명.클래스 변수
-print(Student.class_year)
 
-print(f"# of students: {Student.num_students}")  # of students: 2
+class Mouse(Animal):
+    def speak(self):
+        print("SQUEEK!")
+
+
+dog = Dog("Scooby")
+cat = Cat("Garfield")
+mouse = Mouse("Mickey")
+
+print(dog.name)
+print(cat.name)
+print(mouse.name)
+
+dog.eat()
+cat.eat()
+mouse.eat()
+
+dog.sleep()
+cat.sleep()
+mouse.sleep()
+
+dog.speak()
+cat.speak()
+mouse.speak()
