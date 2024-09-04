058e2556877051087f77e07c957de73379933904
gohdg <gohdg@naver.com>
2024-09-04 10:35:34 +0900

    class variables

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-# object = A "bundle" of related attributes(variables) and methods(functions)
-#          Ex. phone, cup, book
-#          You need a "class" to create many objects
+# class variables = Shared among all instance of a class
+#                   Defined outside the constructor
+#                   Allow you to share data among all objects created from that class
 
-# class = (blueprint) used to design the structure and layout of an object
+class Student:
+    class_year = 2024
+    num_students = 0
 
-from car import Car
+    def __init__(self, name, age) -> None:
+        self.name = name  # instance variable
+        self.age = age  # instance variable
+        Student.num_students += 1
 
-car1 = Car("Mustang", 2024, "red", False)
-car2 = Car("Corvette", 2025, "blue", True)
-car3 = Car("Charger", 2026, "yellow", True)
 
-cars = [car1, car2, car3]
+student1 = Student("Spongebob", 30)
+student2 = Student("Patrick", 35)
 
-# for car in cars:
-#     print(car.model)
-#     print(car.year)
-#     print(car.color)
-#     print(car.for_sale)
-#     print("*****************")
+# class 변수 접근은 intance에서도 접근 되고, 클래스명으로도 접근된다.
+# class 변수 접근법: instance.클래스 변수
+print(student1.name)
+print(student1.age)
+print(student1.class_year)  # 2024, 공유되고 있다
 
-# car1.drive()
-# car1.stop()
-# car2.drive()
-# car2.stop()
+print(student2.name)
+print(student2.age)
+print(student2.class_year)  # 2024, 공유되고 있다
+
+# class 변수 접근법: 클래스명.클래스 변수
+print(Student.class_year)
+
+print(f"# of students: {Student.num_students}")  # of students: 2
