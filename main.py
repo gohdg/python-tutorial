be4273f27ae42a945d468125c6145bb05da805c0
gohdg <gohdg@naver.com>
2024-09-05 18:47:46 +0900

    file detection, os.path.join(), os.path.exists(), os.path.isfile(), os.path.isdir(), os.path.expanduser()

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-# exception = An event thatt interrups the flow of a program
-#            (ZeroDivisionError, TypeError, ValueError)
-#            1.try, 2.except, 3.finally
-# try:
-#     pass
-#     # Try some code
-# except Exception:
-#     pass
-#     # Handle an Exception
-# finally:
-#     pass
-#     # Do some clean up
+# Python file detection
 
+# os.path.exists(file_path)
+# os.path.isfile(file_path)
+# os.path.isdir(file_path)
+# os.path.join(dir, dir, filename)
+# os.path.expanduser("~") // home directry
 
-try:
-    number = int(input("Enter a number: "))
-    print(1/number)
-except Exception as ex:
-    print(f"Error {ex}")
-finally:
-    print("bye")
+import os
+
+home_dir = os.path.expanduser("~")
+
+file_path = home_dir + "/PCCMW/pcc.config"
+file_path = os.path.join(home_dir, "PCCMW", "pcc.config")
+file_path = os.path.join(home_dir, "PCCMW")
+
+print(file_path)
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+else:
+    print("That location doesn't exist")
