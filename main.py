f6de854821c469aba5e259b03480a9988ddcf823
gohdg <gohdg@naver.com>
2024-09-06 10:08:13 +0900

    file read

--- main.py
+++ main.py
@@ -1,72 +1,66 @@
-# Python writing file (.txt, .json, .csv)
-
-# os.path.exists(file_path)
-# os.path.isfile(file_path)
-# os.path.isdir(file_path)
-# os.path.join(dir, dir, filename)
-# os.path.expanduser("~") // home directry
-
-# ******************************
-# * write txt file
-# ******************************
-
-# import os
-
-# home_dir = os.path.expanduser("~")
-# file_path = os.path.join(home_dir, "Desktop", "output.txt")
-
-# employees = ["Eugene", "Squidward", "Spongebob", "Patrick"]
-
-# with open(file=file_path, mode="w") as file:
-#     for employee in employees:
-#         file.write(employee + "\n")
-
-#     print(f"txt file {file_path} was created")
-
-
-# ******************************
-# * write json file
-# * import json
-# * json.dump(object, file stream, indent=4)
-# * json.dump: object를 json string으로 바꾸고 파일저장. indent를 줘서 예쁘게 표현
-# ******************************
-
-# import os
-# import json
-
-# home_dir = os.path.expanduser("~")
-# file_path = os.path.join(home_dir, "Desktop", "output.json")
-
-# employees = {
-#     "name": "Spongebob",
-#     "age": 30,
-#     "job": "cook"
-# }
-
-# with open(file=file_path, mode="w") as file:
-#     json.dump(employees, file, indent=4)
-#     print(f"txt file {file_path} was created")
-
-# ******************************
-# * write csv file (comma seperated value)
-# * import csv
-# * open(, , newline="")을 사용해서 줄바꿈이 추가적으로 생기지 않게 해야한다.
-# ******************************
+# Python reading files (.txt, .json, .csv)
 
 import os
+import json
 import csv
 
 home_dir = os.path.expanduser("~")
-file_path = os.path.join(home_dir, "Desktop", "output.csv")
 
-employees = [["Name", "Age", "Job"],
-             ["Spongebob", 30, "Cook"],
-             ["Patrick", 37, "Unemployed"],
-             ["Sandy", 27, "Scientist"]]
+#  ************************************
+#  * read txt file
+#  * mode="r"
+#  ************************************
 
-with open(file=file_path, mode="w", newline="") as file:
-    writer = csv.writer(file)
-    for row in employees:
-        writer.writerow(row)
+# file_path = os.path.join(home_dir, "Desktop", "output.txt")
 
-    print(f"txt file {file_path} was created")
+# try:
+#     with open(file=file_path, mode="r") as file:
+#         content = file.read()
+#         print(content)
+# except FileNotFoundError:
+#     print("That file was not found")
+# except PermissionError:
+#     print("You do not have permission to read that file")
+# except Exception as ex:
+#     print(f"Error occurred during reading a file: {ex}")
+
+# ************************************
+# * read json file
+# * mode="r"
+# * import json
+# * json.load(file): return object
+# ************************************
+# file_path = os.path.join(home_dir, "Desktop", "input.json")
+# try:
+#     with open(file=file_path, mode="r") as file:
+#         content = json.load(file)
+#         print(content)
+#         print(content["name"])
+# except FileNotFoundError:
+#     print("That file was not found")
+# except PermissionError:
+#     print("You do not have permission to read that file")
+# except Exception as ex:
+#     print(f"Error occurred during reading a file: {ex}")
+
+# ************************************
+# * read csv file
+# * mode="r"
+# * import csv
+# * csv.reader(file): return list collection,한줄 한줄 리스트
+# * csv 파일은 row 에 list가 있다고 생각하면 된다.
+# * 각 list의 item은 column 이 된다.
+# ************************************
+file_path = os.path.join(home_dir, "Desktop", "input.csv")
+try:
+    with open(file=file_path, mode="r") as file:
+        content = csv.reader(file)
+        for line in content:
+            print(line)  # row를 출력
+            print(line[0])  # 각 row의 0번째 column을 출력
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
+except Exception as ex:
+    print(f"Error occurred during reading a file: {ex}")
