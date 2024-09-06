810257859545cf0ed9f11c9158e7f841b70440a5
gohdg <gohdg@naver.com>
2024-09-06 11:27:58 +0900

    multithread

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 
-import datetime
+import threading
+import time
 
-date = datetime.date(2025, 1, 2)
-today = datetime.date.today()
 
-time = datetime.time(12, 30, 0)
-now = datetime.datetime.now()
+def walk_dog(first):
+    time.sleep(8)
+    print(f"You finish walking the {first}")
 
-now = now.strftime("%H:%M:%S %m-%d-%Y")
 
-# print(type(date))
-print(date)
-print(today)
+def take_out_trash():
+    time.sleep(2)
+    print("You take out the trash")
 
-print(time)
-print(now)
 
-target_datetime = datetime.datetime(2026, 1, 2, 12, 30, 1)
-current_datetime = datetime.datetime.now()
+def get_mail():
+    time.sleep(4)
+    print("You get the mail")
 
-if target_datetime < current_datetime:
-    print("Target date has passed")
-else:
-    print("Target date has NOT passed")
+
+# Python 프로그램의 main thread 한개에서 코드가 실행된다. 일반적으로
+# walk_dog()
+# take_out_trash()
+# get_mail()
+
+# args= tuple로 전달 한개일 경우에는 반드시 ,가 있어야 한다
+chore1 = threading.Thread(target=walk_dog, args=("Scooby",))
+chore1.start()
+
+chore2 = threading.Thread(target=take_out_trash)
+chore2.start()
+
+chore3 = threading.Thread(target=get_mail)
+chore3.start()
+
+# .join() 스레드가 종료될때까지 main 스레드가 기다리게 한다
+chore1.join()
+chore2.join()
+chore3.join()
+
+print("All chores are complete")
